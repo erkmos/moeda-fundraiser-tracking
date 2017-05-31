7d7583e929a52570e956761f6226ca245618cd89
Erik Mossberg <lingonvecka@gmail.com>
2017-05-31 18:16:25 +0200

    refactor client events

--- src/app.js
+++ src/app.js
@@ -2,6 +2,21 @@ const tracker = require('./tracker');
 const io = require('socket.io')();
 const logger = require('./logger');
 
+async function handleClientAction(client, action) {
+  const result = { type: null };
+
+  switch (action.type) {
+    case 'server/balance':
+      result.data = await tracker.getBalance(action.data);
+      result.type = 'balance';
+      break;
+    default:
+      return;
+  }
+
+  client.emit('action', result);
+}
+
 async function handleConnection(client) {
   try {
     const state = await tracker.getCurrentState();
@@ -11,10 +26,11 @@ async function handleConnection(client) {
     return;
   }
 
-  client.on('balance', async (address) => {
-    const balance = await tracker.getBalance(address);
-    client.emit('balance', { balance });
-  });
+  client.on('action', handleClientAction.bind(null, client));
+}
+
+function makeAction(data) {
+  return { type: 'FUNDRAISER_UPDATE', data };
 }
 
 async function run(contractAddress, topic) {
@@ -24,13 +40,16 @@ async function run(contractAddress, topic) {
   io.listen(3000, () => logger.info('Listening on port 3000'));
 
   trackerEvents.on('error', (message) => logger.error(message));
-  trackerEvents.on('block', (height) => io.sockets.emit(
-    'update', { blockNumber: height }));
   trackerEvents.on('purchase', (message) => logger.info(message));
-  trackerEvents.on('update', (total) => io.sockets.emit(
-    'update', { totalReceived: total }));
   trackerEvents.on(
-    'rate', (rate) => io.sockets.emit('update', { exchangeRate: rate }));
+    'block',
+    (height) => io.sockets.emit('action', makeAction({ blockNumber: height })));
+  trackerEvents.on(
+    'update',
+    (total) => io.sockets.emit('action', makeAction({ totalReceived: total })));
+  trackerEvents.on(
+    'rate',
+    (rate) => io.sockets.emit('action', makeAction({ exchangeRate: rate })));
 }
 
 module.exports = {
