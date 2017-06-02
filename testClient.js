874ded2092944cdf4149789752b744608656adf7
Erik Mossberg <lingonvecka@gmail.com>
2017-06-02 18:27:00 +0200

    add echo testclient

---
+++ testClient.js
@@ -0,0 +1,18 @@
+const socket = require('socket.io-client');
+const {
+  CLIENT_BALANCE_RESULT,
+  ERROR_EVENT,
+  TOTAL_RECEIVED_EVENT,
+  CLIENT_ACTION_EVENT,
+} = require('./src/constants');
+
+const client = socket('ws://localhost:3000');
+client.on('connect', () => {
+  console.log('connected to server');
+  client.emit('balance', '0xd521e018e611e73c3b8d17d7f4359acd0ff22ea1');
+});
+client.on(CLIENT_ACTION_EVENT, (data) => console.log(data));
+client.on(TOTAL_RECEIVED_EVENT, (data) => console.log(data));
+client.on('disconnect', () => console.log('server disconnected'));
+client.on(CLIENT_BALANCE_RESULT, (data) => console.log(data));
+client.on(ERROR_EVENT, (error) => console.error(error));
