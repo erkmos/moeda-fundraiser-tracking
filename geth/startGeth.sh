c09f22f3017b60e6ba3554df22cbbba22cccc794
Erik Mossberg <lingonvecka@gmail.com>
2017-06-02 19:15:23 +0200

    get rid of modules breaking build

--- geth/startGeth.sh
+++ geth/startGeth.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-geth --fast --rpc --rpcaddr 0.0.0.0 --rpcapi personal,eth,net,web3 --ws --wsorigins "*" --wsaddr 0.0.0.0 geth --datadir /data
+geth --ropsten --fast --rpc --rpcaddr 0.0.0.0 --rpcapi personal,eth,net,web3 --ws --wsorigins "*" --wsaddr 0.0.0.0 geth --datadir /data
