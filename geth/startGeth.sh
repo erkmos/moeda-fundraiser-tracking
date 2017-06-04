c2773955c7dd8b9ae9e6f4568ac39d5021ff9f4a
Erik Mossberg <lingonvecka@gmail.com>
2017-06-04 20:10:48 +0200

    increase cache size for geth

--- geth/startGeth.sh
+++ geth/startGeth.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-geth --fast --cache=1024 --rpc --rpcaddr 0.0.0.0 --rpcapi personal,eth,net,web3 --ws --wsorigins "*" --wsaddr 0.0.0.0 geth --datadir /data
+geth --fast --cache=4096 --rpc --rpcaddr 0.0.0.0 --rpcapi personal,eth,net,web3 --ws --wsorigins "*" --wsaddr 0.0.0.0 geth --datadir /data
