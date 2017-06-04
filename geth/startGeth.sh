b062fa76ab7b4e7359e1201da457961f32626bd5
Erik Mossberg <lingonvecka@gmail.com>
2017-06-04 19:51:35 +0200

    increase geth cache

--- geth/startGeth.sh
+++ geth/startGeth.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-geth --syncmode fast --rpc --rpcaddr 0.0.0.0 --rpcapi personal,eth,net,web3 --ws --wsorigins "*" --wsaddr 0.0.0.0 geth --datadir /data
+geth --fast --cache=1024 --rpc --rpcaddr 0.0.0.0 --rpcapi personal,eth,net,web3 --ws --wsorigins "*" --wsaddr 0.0.0.0 geth --datadir /data
